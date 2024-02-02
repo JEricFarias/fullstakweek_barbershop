f136a2ac6607789ff6d62e698b32a56c70e32d99
JEricFarias <ericfarrias@hotmail.com>
2024-02-02 00:21:52 -0300

    feat: welcome message

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,10 +1,18 @@
-import Image from "next/image";
+import { format } from "date-fns";
 import Header from "../_components/header";
+import { ptBR } from "date-fns/locale";
 
 export default function Home() {
   return (
     <div>
       <Header />
+
+      <div className="px-5 pt-5">
+        <h2 className="text-xl font-bold">Olá, Eric</h2>
+        <p className="text-sm capitalize">
+          {format(new Date(), "EEEE',' dd 'de' MMMM", { locale: ptBR })}
+        </p>
+      </div>
     </div>
   );
 }
